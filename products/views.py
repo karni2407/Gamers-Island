0034a122907b074dc04a9e93ba160f60d9646c3e
karni2407 <karnisingh2407@gmail.com>
2026-08-20 18:57:58 +0530

    Added paginator and improved functionality

--- products/views.py
+++ products/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+
 from .models import Product
 
 
 def products(request):
-    products = Product.objects.all()
+
+    product_list = Product.objects.all()
+
+    paginator = Paginator(product_list, 8)
+
+    page_number = request.GET.get("page")
+    products = paginator.get_page(page_number)
 
     return render(request, "product.html", {
         "products": products
@@ -11,6 +19,7 @@ def products(request):
 
 
 def product_details(request, slug):
+
     product = get_object_or_404(Product, slug=slug)
 
     return render(request, "product_details.html", {
